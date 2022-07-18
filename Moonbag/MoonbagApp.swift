2891461e249d62c4677363affc2cbc28d3bcd7b6
Minh Pham <pcminh0505@gmail.com>
2022-07-18 22:41:42 +0700

    Initial Commit

---
+++ Moonbag/MoonbagApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonbagApp.swift
+//  Moonbag
+//
+//  Created by Minh Pham on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonbagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
